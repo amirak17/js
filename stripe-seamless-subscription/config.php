e9bd0437707bc32b9e513365556ee0b89e0db6d0
Amir Khan <amir.aakhan@gmail.com>
2022-02-09 19:16:48 -0500

    update

---
+++ stripe-seamless-subscription/config.php
@@ -0,0 +1,12 @@
+<?php 
+
+// $s_key = 'sk_xxxx_xxxx';
+$s_key = 'sk_test_51IBqdOFUVpeXrD7kgzhPc4TBaT52ENk45pokIw0XSlBaZSSkvPUUSN6QRloyOitf1GUkQ63EA2ecgaC9vEWjF7Cl00stjskSCr';
+
+// $p_key = 'pk_xxxx_xxxx';
+$p_key = 'pk_test_51IBqdOFUVpeXrD7kNXTXdnPR0HGFWboXK4G2D2hEpM3hfphRudcgHXMV9eKYvgnm0UGYEvjNWDLL2Z9agWotZkGr00O6WobEP6';
+
+// $price_code = 'price_xxxx';
+$price_code = 'price_1IBrD3FUVpeXrD7k4aj2zYE3';
+
+?>
